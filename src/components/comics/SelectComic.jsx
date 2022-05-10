a778c0c4789879d39fee14c2e12db9b9fa87622d
Nicole Steadman <nsteadman@gmail.com>
2022-05-09 20:23:34 -0700

    added SelectComic component to build out dropdown menu

--- src/components/comics/SelectComic.jsx
+++ src/components/comics/SelectComic.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { headers } from '../../Globals';
+import { baseUrl } from '../../Globals';
 
 const SelectComic = ({ comic }) => {
+    const [id, setId] = useState("");
+    const [comics, setComics] = useState([]);
+
+    const addComicOption = comic => {
+        setComics([...comics, comic]);
+      }
+
+    const handleClick = e => {
+        e.preventDefault();
+        const params = {
+            comic: {
+                comic_id: id
+            }
+        }
+
+        fetch(baseUrl + "/user_comics", {
+            method: "POST",
+            headers,
+            body: JSON.stringify(params)
+        })
+            .then(r => r.json())
+            .then(data => {
+                addComicOption(data);
+            })
+    }
   return (
-    <div >
+    <div onClick={ handleClick }>
         { comic.name }
     </div>
   )
 }
 
-export default SelectComic
+export default SelectComic;
